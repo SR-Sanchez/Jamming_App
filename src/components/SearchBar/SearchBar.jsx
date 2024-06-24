f3b465e9551e25a236a3d6f3db2b1e1011367261
Sergio Ruiz <sergioruizsanchezz@gmail.com>
2024-06-24 18:28:17 -0500

    search bar static working

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+import { useState } from "react";
+
+const SearchBar = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const handleTermSearch = event => {
+		setSearchTerm(event.target.value)
+	};
+
+	const handleSubmit = () => { //mockup function as prove of concept - needs to be changed
+    alert(JSON.stringify(searchTerm));
+		setSearchTerm("")
+  };
+
+
+
+	return (
+		<>
+			<input placeholder="Search for a song" value={searchTerm} onChange={handleTermSearch}></input>
+			<button onClick={handleSubmit}>Search</button>
+		</>
+	);
+};
+
+export default SearchBar
